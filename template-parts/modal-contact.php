2fcacb18a85515f48bb7c223c5a14720656f86fa
juliette <jujujack218956@gmail.com>
2026-05-26 11:41:04 +0200

    feat: styles champs formulaire CF7 et ajustements modale contact

--- template-parts/modal-contact.php
+++ template-parts/modal-contact.php
@@ -1,7 +1,7 @@
 <div class="modal-overlay">
   <div class="modal-contact">
     <div class="modal-header">
-      <p aria-hidden="true">ContactContactContactCont<br>actContactContactContact</p>
+      <p aria-hidden="true">ContactContactContactContactContactContactContact<br>actContactContactContactactContactContactContact</p>
     </div>
     <?php echo do_shortcode('[contact-form-7 id="2dba1f4" title="Formulaire de contact"]'); ?>
 
